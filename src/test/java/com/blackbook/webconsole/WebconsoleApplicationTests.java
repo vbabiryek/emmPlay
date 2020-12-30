bf47b85ceb8bb0253c519dc203aaa36396f19dd2
vbabiryek <vbabiryek@gmail.com>
2020-12-30 16:04:59 -0500

    added tests

--- src/test/java/com/blackbook/webconsole/WebconsoleApplicationTests.java
+++ src/test/java/com/blackbook/webconsole/WebconsoleApplicationTests.java
@@ -1,19 +1,39 @@
 package com.blackbook.webconsole;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.blackbook.webconsole.controllers.urls.Urls;
+import com.blackbook.webconsole.services.EnterpriseI;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class WebconsoleApplicationTests {
 
+	@Autowired
+	public EnterpriseI enterpriseImpl;
+	
+	@Autowired
+	public MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
-		
+	void visitHomepage() throws Exception {
+		mockMvc.perform(get(Urls.GET_COSU_POLICY)).andDo(print()).andExpect(status().isOk());
 	}
 	
 	@Test
 	void urlTesting() {
-		assertEquals(1,1);
+		assertThat(enterpriseImpl).isNotNull();
 	}
+	
+	//Mock MVC - inject dependencies through REST (Autowires) - visit the URLs
 
 }
